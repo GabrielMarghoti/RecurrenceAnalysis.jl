c4e09fc82639a7767431ba75401f51e4ec95f8be
Gabriel Marghoti <gabrielmarghoti@gmail.com>
2025-04-22 23:50:36 +0200

    colunmwise upper lower regions

--- src/rqa/histograms.jl
+++ src/rqa/histograms.jl
@@ -334,6 +334,8 @@ function motifshistogram(R::Union{ARM,AbstractMatrix}, L::Union{Int, Tuple{Int, 
     elseif sampling == :columnwise
         # Column-wise sampling
         for i in xrange            
+            if (i<L[1]) || (i > N - L[1]) || (i + L[2] > N)
+                continue
             if sampling_region == :lower
                 yrange = max(1, -(L[2]-1)):(i)
             elseif sampling_region == :upper
