0eff4c76e033203a10248c57d3bff35eb7b7a72f
Gabriel Marghoti <gabrielmarghoti@gmail.com>
2025-04-22 16:25:10 +0200

    bug on colunmwise

--- src/rqa/histograms.jl
+++ src/rqa/histograms.jl
@@ -252,6 +252,10 @@ Calculate the probabilities of motifs from a given recurrence matrix `R` and a m
 function motifshistogram(R::Union{ARM,AbstractMatrix}, L::Union{Int, Tuple{Int, Int}}; shape::Symbol=:square, sampling::Symbol=:full, sampling_region::Symbol=:all, num_samples::Union{Int,Float64}=1.0)
     N = size(R, 1)
 
+    if typeof(L) == Int
+        L = (L, L)
+    end
+
     if shape == :square
         num_motifs = 2^(L[1] * L[1])
     elseif shape == :triangle
@@ -261,11 +265,6 @@ function motifshistogram(R::Union{ARM,AbstractMatrix}, L::Union{Int, Tuple{Int, 
     else
         throw(ArgumentError("Invalid shape. Use :timepair, :square or :triangle."))
     end
-
-    if typeof(L) == Int
-        L = (L, L)
-    end
-
 
     dh = zeros(num_motifs)
 
@@ -336,6 +335,7 @@ function motifshistogram(R::Union{ARM,AbstractMatrix}, L::Union{Int, Tuple{Int, 
         # Column-wise sampling
         for i in xrange
             for j in yrange
+                print("i: $i, j: $j\n")
                 if (j == i) || (i + L[1] == j + L[2])
                     continue
                 end
